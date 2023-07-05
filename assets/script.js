cca0acd4f1e729bcee5222d60f46a1f175cd0450
Scott Keller <smkdbv911@gmail.com>
2023-07-04 21:44:21 -0600

    added jquery function, event listeners and function scope vars

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,14 @@
-  // Add a listener for click events on the save button
-  // Save the user input in local storage using the time-block id as the key
+$(function() {
+ // Add a listener for click events on the save button
+  $(".saveBtn").on("click", function() {
+  
   // Get the user input description from the textarea
+  var description = $(this).siblings(".description").val().trim();
   // Get the id of the containing time-block
+  var timeBlockId = $(this).parent().attr("id");
+ // Save the user input in local storage using the time-block id as the key
+  localStorage.setItem(timeBlockId, description);
+  });
   // Apply the past, present, or future class to each time block
   // Get the current hour 
   // Loop through each time-block
@@ -9,3 +16,4 @@
   // Call the function to update hour classes
   // Get any user input saved in localStorage and set the textarea values
   // Display the current date in the header of the page
+});
